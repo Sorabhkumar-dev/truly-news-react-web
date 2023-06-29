936c60b14df59e26f0630aa438f053b553bad65c
Sorabh kumar <sorabhkumardeveloper@gmail.com>
2023-06-29 14:25:03 +0530

    chore:completed top headline page

--- src/componets/TopHeadline.js
+++ src/componets/TopHeadline.js
@@ -2,25 +2,46 @@ import React from 'react'
 import NewsCard from './NewsCard'
 import { useEffect, useState } from 'react';
 import { getTopHeadline } from '../utils/Network';
+import {PAGE_SIZE} from '../utils/Constants'
 
 export default function TopHeadline() {
     const [articles, setArticle] = useState([]);
+    const [page,setPage] = useState(0)
+
+    const handlePrevBtnClicked = () => {
+      if(page != 0){
+        setPage(page-1)
+      }  
+    }
+
+    const handleNextBtnClicked = () => {
+      console.log(`page -> ${page}`)
+      if(articles.length % PAGE_SIZE == 0){
+        setPage(page+1)
+      }
+    }
 
    useEffect(() =>{
-    getTopHeadline().then( response =>{
+    getTopHeadline(page).then( response =>{
         console.log(response.data.articles)
         setArticle(response.data.articles)
          
    }).catch(error =>{
     console.log(error)
     
-   })},[])
+   })},[page])
 
   const newsItems = articles.map((article)=>{
    return <NewsCard key={article.url} article={article}/>
    })
 
   return (
+    <>
     <div className='d-flex flex-wrap justify-content-around'>{newsItems}</div>
+    <div className='d-flex justify-content-between mx-3 mt-3 mb-5'>
+      <button type="button" className="btn btn-primary" onClick={handlePrevBtnClicked}>&laquo; Prev</button>
+      <button type="button" className="btn btn-primary" onClick={handleNextBtnClicked}>Next &raquo;</button>
+    </div>
+    </>
   )
 }
--- src/utils/Network.js
+++ src/utils/Network.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 const NEWS_BASE_URL = "https://newsapi.org/v2/"
 
-export const getTopHeadline = async () => await axios.get(`${NEWS_BASE_URL}/top-headlines?country=in&category=business&apiKey=f5cd498bf1ed4abaa3acd25694c95808`);
+export const getTopHeadline = async (page) => await axios.get(`${NEWS_BASE_URL}/top-headlines?country=in&category=business&page=${page}&apiKey=f5cd498bf1ed4abaa3acd25694c95808`);
 
   
